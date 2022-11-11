24e7e69069799b57e77ee3704c709f425c0b5131
edentibebu <etibebu@wustl.edu>
2022-11-10 23:37:31 -0600

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+alert("this is a message from the future");
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello World!</h1>);
